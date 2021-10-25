f10b6d1b0d589f6aa0132211d3f295620c0ed0ee
Alessandro Ruggieri <alessandro.ruggieri@roma1.infn.it>
2021-10-25 10:26:53 +0200

    fix logger

--- api/logger.js
+++ api/logger.js
@@ -6,7 +6,7 @@ const {sendMail} =require("./mailer")
 const {getUser} = require('./user') 
 
 
-const dump=(fileName,data)=>{
+/*const dump=(fileName,data)=>{
 
     let path=__dirname+`/../logs/${fileName}.txt`
     let content="==================================\n"
@@ -16,37 +16,25 @@ const dump=(fileName,data)=>{
     
     fs.appendFileSync(path,content)
 
-}
+}*/
 
-router.post("/",(req,res)=>{
+router.post("/", async (req,res)=>{
     let user=req.body.user;
     
     user= await getUser(user.uuid)
 
     if(!user) return  res.status(500).json({"err":`No user found`})
-
-    if(user.isAuthorized) return res.json({"txt":`user is ok`})
-
+    let status = user.isAuthorized ? "ok" : "bad"
+    let subject=`form-web - ${status} user status: ${user.name} ${user.surname}`
+    let from='supporto@roma1.infn.it'
+    let to=from;
+   
     const {templates}=require("./templates")
 
-    let txt=`
-    
-        <pre>
-        ${templates.userInfo(user)}
-        -----------------------------------------------------------------
-        Autorizzato     : ${user.isAuthorized ? 'SI': 'NO'}    
-        -----------------------------------------------------------------
-        Ruolo roma1     : ${user.role ? 'SI' : 'NO'}
-        LOA2            : ${user.loa2 ? 'SI' : 'NO'}
-        Disciplinare    : ${user.policies ? 'SI' : 'NO'}
-        Corso sicurezza : ${user.itsec ? 'SI' : 'NO'}
-        GraceTime       : ${user.gracetime ? 'SI' : 'NO'}
-        </pre>
-        
-    `
-        
-    sendMail('supporto@roma1.infn.it','supporto@roma1.infn.it',	`form-web - bad user status: ${user.name} ${user.surname}`,txt).then(res=>{
-        console.log(`${user.name} ${user.surname} missing login requirements`)
+    let txt=`<pre>${templates.completeUserInfo(user)}</pre>`
+     
+    sendMail(from,to,subject,txt).then(res=>{
+        console.log(`${subject}`)
     }).catch(err=>{
         console.log(err);
     }).finally(_=>{
